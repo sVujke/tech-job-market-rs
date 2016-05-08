de0e45cae7b7f90a636dee0367bf3b3dacdf4fe7
vujke <stefanvujovic93@gmail.com>
2016-05-08 19:27:38 +0200

    methods added

--- scrapers/startit.py
+++ scrapers/startit.py
@@ -2,19 +2,55 @@ from bs4 import BeautifulSoup
 import requests, time
 import scrape_lib
 
+start = time.time()
+
 # f = open("C:/Users/vujke/Documents/GitHub/tech-job-market-rs/data/data.txt" ,"a")
 # errorFile = open("C:/Users/vujke/Documents/GitHub/tech-job-market-rs/data/error.txt" ,"w")
 
 # scraper methods
 
-# methods for premium ads
-
-# methods for standard ads
+# methods for PREMIUM and STANDARD ads
+# PARAMETER po is a single ad div
+def get_city(po):
+    firm_city = po.find_all('div',class_="listing-ime-firme")
+    city = firm_city[1].get_text()
+    city = city.split(' ')[3:]
+    cities = ""
+    for c in city:
+        cities = cities+str(c)
+    return cities
+# PARAMETER ad is a list of all <a> tags
 def get_position(ad):
     return ad[1].get_text()
 
-# methods for mini ads
-start = time.time()
+def get_firm(ad):
+    return ad[2].get_text()
+
+def get_tags(ad):
+    tags = ad[3:]
+    tag_str = ""
+    for tag in tags:
+        tag_str = tag_str + str(tag.get_text())+" "
+    tag_str = tag_str.lower()
+    return tag_str
+# methods for MINI ads
+def get_city_s(ad):
+    return ad[1].get_text()
+
+def get_position_s(ad):
+    return ad[0].get_text()
+
+def get_tags_s(ad):
+    tags = ad[2:]
+    tag_str = ""
+    for tag in tags:
+        tag_str = tag_str + str(tag.get_text())+" "
+    tag_str = tag_str.lower()
+    return tag_str
+
+def get_firm_s(po):
+    return po.find('div',class_="oglas-mini-header").get_text()
+
 
 #base URL
 url="http://startit.rs/poslovi/"
